d974bc414378bc9f7ed5f2d8340008481f071a94
Varshini <varshuk01@gmail.com>
2022-10-18 21:36:25 +0530

    admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myrouter:Router) { }
+
+     username=""
+
+     password=""
+
+status:boolean=false
+
+     readValues=()=>{
+
+      let data={
+
+        "username":this.username,
+
+        "password":this.password
+
+      }
+
+      if(this.username=="admin" && this.password=="admin"){
+
+        this.myrouter.navigate(["/collegedash"])
+
+      }else {
+
+        alert("invalid credential")
+
+      }
+
+      console.log(data)
+
+      this.status=true
+
+     }
+
+
 
   ngOnInit(): void {
   }
