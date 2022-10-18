fd67470d8f96c2777c9132b646f2043cc21418bb
Varshini <varshuk01@gmail.com>
2022-10-18 22:29:14 +0530

    vieww all student

--- src/app/viewallstudent/viewallstudent.component.ts
+++ src/app/viewallstudent/viewallstudent.component.ts
@@ -9,6 +9,8 @@ export class ViewallstudentComponent implements OnInit {
 
   constructor() { }
 
+  fetchData=[{"name":"varshini","rollno":101,"department":"CSE","dob":"07.01.2001"},{"name":"jenny","rollno":102,"department":"ECE","dob":"10.01.2001"},{"name":"khyny","rollno":103,"department":"CSE","dob":"12.04.2000"},{"name":"jerry","rollno":104,"department":"IT","dob":"17.10.2000"},{"name":"nega","rollno":105,"department":"CSE","dob":"27.11.2000"}]
+
   ngOnInit(): void {
   }
 
